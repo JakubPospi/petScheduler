1f5a3b4a37de99f18df7df95e952599d60ffe369
Adam Mihok <a.mihok.st@spseiostrava.cz>
2026-05-20 12:10:29 +0200

    feat: login uzivatele, idk zda li je funkcni

--- login.php
+++ login.php
@@ -3,25 +3,45 @@ require "./utils/init.php";
 $chyba = false;
 
 if (isset($_POST['username'])) {
+
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $passHASH = sha1($password); 
-    $stmt = $db->prepare("SELECT * FROM users WHERE username = ? AND passwd = ?");
-    $stmt->bind_param("ss", $username, $passHASH);
-    
-    $stmt->execute();
-    $result = $stmt->get_result();     
-    $uzivatel = $result->fetch_assoc(); 
 
-    if ($uzivatel) {
-        $_SESSION['uzivatel'] = $uzivatel['username'];
-        header("Location: index.phhtml");
+    $stmt = mysqli_prepare($db, "
+        SELECT * FROM users
+        WHERE username = ?
+    ");
+
+    if ($stmt === false) {
+        echo "<h1>Přihlášení se nezdařilo.</h1>";
+        echo mysqli_error($db);
         exit;
-    } 
-    else {
+    }
+    mysqli_stmt_bind_param($stmt, "s", $username);
+
+    $result = mysqli_stmt_execute($stmt);
+
+    if ($result === false) {
+        echo "<h1>Přihlášení se nezdařilo.</h1>";
+        echo mysqli_error($db);
+        exit;
+    }
+
+    $result = mysqli_stmt_get_result($stmt);
+    $uzivatel = mysqli_fetch_assoc($result);
+
+    if ($uzivatel && password_verify($password, $uzivatel['passwd'])) {
+
+        $_SESSION['uzivatel'] = $uzivatel['username'];
+
+        header("Location: index.php");
+        exit;
+
+    } else {
         $chyba = true;
     }
-    $stmt->close();
+
+    mysqli_stmt_close($stmt);
 }
 
 if ($chyba) echo "<b>Chybné přihlašovací údaje!</b><br>"; 
