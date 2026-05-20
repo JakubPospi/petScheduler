0a09a215b4f49b342698f3c370adcd7080863db6
Jakub Pospisil <j.pospisil.st@spseiostrava.cz>
2026-05-20 09:40:39 +0200

    login hotovy chybi register

--- login.php
+++ login.php
@@ -1,5 +1,33 @@
 <?php
+require "./utils/init.php";
+$chyba = false;
+
+if (isset($_POST['username'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $passHASH = sha1($password); 
+    $stmt = $db->prepare("SELECT * FROM users WHERE username = ? AND passwd = ?");
+    $stmt->bind_param("ss", $username, $passHASH);
+    
+    $stmt->execute();
+    $result = $stmt->get_result();     
+    $uzivatel = $result->fetch_assoc(); 
+
+    if ($uzivatel) {
+        $_SESSION['uzivatel'] = $uzivatel['username'];
+        header("Location: index.phhtml");
+        exit;
+    } 
+    else {
+        $chyba = true;
+    }
+    $stmt->close();
+}
+
 
 require "./layout/header.phtml";
 require "./login.phtml";
 require "./layout/footer.phtml";
+
+
+?>
