ff594fe1549fc42e00086e6b9f7dbc53fe628a78
Adam Mihok <a.mihok.st@spseiostrava.cz>
2026-05-20 12:32:31 +0200

    pridani podminky pro kontrolu zda li je uzivatel prihlasen tak se zobrazi druhy header

--- dashboard.php
+++ dashboard.php
@@ -2,7 +2,11 @@
 require "./utils/init.php";
 $uzivatel = $_SESSION['uzivatel'];
 
-
+if($uzivatel ?? null){
 require "./layout/header.phtml";
+}
+else{
+    require "./layout/header2.phtml";
+}
 require_once "./dashboard.phtml";
 require "./layout/footer.phtml";
