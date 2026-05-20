3a45181e1dd8c7cd692965880c4392912fdaf6cc
Jakub Pospisil <j.pospisil.st@spseiostrava.cz>
2026-05-20 13:08:12 +0200

    nehotovy vypis rodiny na strance dashboard

--- dashboard.php
+++ dashboard.php
@@ -2,6 +2,20 @@
 require "./utils/init.php";
 $uzivatel = $_SESSION['uzivatel'];
 
+$nazev_rodiny = "Žádná rodina"; 
+
+if ($uzivatel !== null && !empty($uzivatel["family_id"])) {
+    // V SELECTu taháme 'name', protože se tak jmenuje sloupec v tabulce family
+    $stmt = $db->prepare("SELECT family_name FROM family WHERE ID = ?");
+    $stmt->execute([$uzivatel["family_id"]]);
+    $rodina = $stmt->fetch();
+    
+    if ($rodina) {
+        // Tady musíme použít ['name'], protože to jsme vybrali v SQL dotazu
+        $nazev_rodiny = $rodina["name"]; 
+    }
+}
+
 if($uzivatel === null){
 require "./layout/header.phtml";
 }
