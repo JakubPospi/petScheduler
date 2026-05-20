8671ef9a30a7177a04484393b2726d0862bc0f69
Jakub Pospisil <j.pospisil.st@spseiostrava.cz>
2026-05-20 13:13:11 +0200

    odstraneni komentu z dashboard

--- dashboard.php
+++ dashboard.php
@@ -5,13 +5,12 @@ $uzivatel = $_SESSION['uzivatel'];
 $nazev_rodiny = "Žádná rodina"; 
 
 if ($uzivatel !== null && !empty($uzivatel["family_id"])) {
-    // V SELECTu taháme 'name', protože se tak jmenuje sloupec v tabulce family
+
     $stmt = $db->prepare("SELECT family_name FROM family WHERE ID = ?");
     $stmt->execute([$uzivatel["family_id"]]);
     $rodina = $stmt->fetch();
     
     if ($rodina) {
-        // Tady musíme použít ['name'], protože to jsme vybrali v SQL dotazu
         $nazev_rodiny = $rodina["family_name"]; 
     }
 }
