87903f9ae0be8a2356176dba6d6ee178a3152ef1
Adam Mihok <a.mihok.st@spseiostrava.cz>
2026-05-26 09:10:36 +0200

    pridani kolonky do db is_done a pridavani zda li je ukol splnen

---
+++ complete_task.php
@@ -0,0 +1,15 @@
+<?php
+require "./utils/init.php";
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['task_id'])) {
+    $taskId = (int)$_POST['task_id'];
+ 
+    $stmt = $conn->prepare("UPDATE tasks SET is_done = 1 WHERE id = ?");
+    $stmt->bind_param("i", $taskId);
+    $stmt->execute();
+
+}
+header("Location: dashboard.php"); 
+exit;
+?>
