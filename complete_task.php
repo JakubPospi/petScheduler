6802a67ff46ce5ff2311bdba542001ef6af8cf3a
Adam Mihok <a.mihok.st@spseiostrava.cz>
2026-05-26 09:18:48 +0200

    spravny vypis stavu dashboardu a funkcni tlacitko splnit

--- complete_task.php
+++ complete_task.php
@@ -5,7 +5,7 @@ require "./utils/init.php";
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['task_id'])) {
     $taskId = (int)$_POST['task_id'];
  
-    $stmt = $conn->prepare("UPDATE tasks SET is_done = 1 WHERE id = ?");
+    $stmt = $db->prepare("UPDATE tasks SET is_done = 1 WHERE id = ?");
     $stmt->bind_param("i", $taskId);
     $stmt->execute();
 
