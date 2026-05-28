8ad37d631254a06ca5be6e405f5aacbc1f7aa99d
Jakub Pospisil <j.pospisil.st@spseiostrava.cz>
2026-05-28 09:24:32 +0200

    vypis jmena a emailu ve formulari upravy dat clenu rodiny

--- family.php
+++ family.php
@@ -7,7 +7,7 @@ if ($uzivatel === null || empty($uzivatel["family_id"])) {
 }
 
 
-$stmt = $db->prepare("SELECT username, role_id, id FROM users WHERE family_id = ?");
+$stmt = $db->prepare("SELECT username, role_id, id, email FROM users WHERE family_id = ?");
 $stmt->execute([$uzivatel["family_id"]]);
 
 $clenove = $stmt->get_result(); 
@@ -17,11 +17,6 @@ $rod->execute([$uzivatel["family_id"]]);
 
 $vysledek_rodiny = $rod->get_result()->fetch_assoc();
 $nazev_rodiny = $vysledek_rodiny['family_name'];
-
-$user_to_edit_id = intval($_POST['user_to_edit']);
-$vypis = $db->prepare("SELECT u.username, u.email, u.role_id, r.name FROM users u LEFT JOIN role_type r ON u.role_id = r.id_role WHERE u.id = ?");
-$vypis->execute([$user_to_edit_id]);
-$profil = $vypis->get_result()->fetch_assoc();
 
 
 require "./layout/header2.phtml";
